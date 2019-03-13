20b06fef0e1779af6c9c8386c22b7febf956c6a9
Galina Is <denis@isyanov.com>
2019-03-13 19:04:16 +0300

    Lesson5.

--- Lesson5/src/Lesson5.java
+++ Lesson5/src/Lesson5.java
@@ -1,11 +1,49 @@
 import com.sbt.javaschool.lesson5.*;
 
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
 public class Lesson5 {
     public static void main(String[] args) {
+        //testTerminal();
+        testUrl();
+    }
+
+    private static void testTerminal() {
         try {
             TerminalImpl terminal = new TerminalImpl(5000, "1111");
             terminal.work();
         } catch (PinValidatorException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private static void testUrl() {
+        boolean isRead = false;
+        Scanner in = new Scanner(System.in);
+        String urlString = "";
+        while (!isRead) {
+            try {
+                System.out.print("Введите URL ресурса: ");
+                urlString = in.nextLine();
+                readContent(urlString);
+                isRead = true;
+            } catch (MalformedURLException ex) {
+                System.out.println("Не удалось получить доступ к URL: " + urlString);
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    private static void readContent(String urlString) throws MalformedURLException {
+        URL url = new URL(urlString);
+        try (InputStream in = new BufferedInputStream(url.openStream()); Reader r = new InputStreamReader(in)) {
+            int c;
+            while ((c = r.read()) != -1) {
+                System.out.print((char) c);
+            }
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
